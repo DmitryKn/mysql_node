d4076af9fd73f49d9e7af8564e2abd27abd97b9b
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-06-13 12:25:50 -0400

    add email to database

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const faker = require('faker');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
+//DB config
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -13,7 +15,10 @@ con.connect(err => {
   if (err) throw err;
   console.log("DB Connected!");
 });
-
+//APP CONFIG
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 //ROUTES
 app.get("/", (req, res) => {
@@ -21,10 +26,20 @@ app.get("/", (req, res) => {
   con.query(q, (err, result) => {
     if(err) throw err;
     var count = result[0].count;
-    res.send("We have " + count + " users in our database.")
+    //res.send("We have " + count + " users in our database.")
+    res.render('home.ejs', {data: count})
+  })
+})
+app.post('/register',(req, res)=>{
+  var person = {email: req.body.email} //store post body.
+  con.query("INSERT INTO users SET ?", person, (err, result) => {
+    if(err) throw error;
+    console.log(result);
+  res.redirect('/')
   })
 })
 
+//APP listen
 app.listen(3000, () => {
   console.log("Server started. Port 3000");
 })
